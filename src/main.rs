4f38641a41fad958a821a88b66e08d734f03e352
Jeremy Kerr <jk@codeconstruct.com.au>
2023-06-03 18:59:09 +0800

    Print device info as proper format
    We want to output the full device info, add a proper format for this,
    rather than plain Debug.
    
    Signed-off-by: Jeremy Kerr <jk@codeconstruct.com.au>

--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,68 @@ mod mctp;
 mod pldm;
 mod pldm_fw;
 
+use enumset::{EnumSet, EnumSetType};
+use std::fmt::Write;
+
 const EID: u8 = 0x09;
+
+fn comma_separated<T: EnumSetType + std::fmt::Debug>(e: EnumSet<T>) -> String {
+    let mut s = String::new();
+    let mut first = true;
+    for i in e.iter() {
+        write!(s, "{}{:?}", if first { "" } else { "," }, i).unwrap();
+        first = false;
+    }
+    s
+}
+
+fn print_device_info(
+    dev: &pldm_fw::DeviceIdentifiers,
+    fwp: &pldm_fw::FirmwareParameters,
+) {
+    println!("Device: {}", dev);
+    println!("Firmware Parameters:");
+    println!("  Active version:  {}", fwp.active);
+    println!("  Pending version: {}", fwp.pending);
+    println!(
+        "  Update caps: [0x{:x}]:{}",
+        fwp.caps.as_u32(),
+        if fwp.caps.is_empty() { " none" } else { "" }
+    );
+    for cap in fwp.caps.iter() {
+        println!("    * {:?}", cap);
+    }
+    println!(
+        "  Components:{}",
+        if fwp.components.is_empty() { " none" } else { "" }
+    );
+    for (idx, comp) in fwp.components.iter().enumerate() {
+        println!("    [{}]", idx);
+        println!("      Classification:  {:?}", comp.classification);
+        println!("      Index:           {:?}", comp.classificationindex);
+        println!("      Identifier:      0x{:04x}", comp.identifier);
+        println!("      Active Version:  {}", comp.active);
+        println!("      Pending Version: {}", comp.pending);
+        println!(
+            "      Activation:      [0x{:x}] {}",
+            comp.activation_methods.as_u32(),
+            comma_separated(comp.activation_methods)
+        );
+        println!(
+            "      Update caps:     [0x{:x}] {}",
+            comp.caps_during_update.as_u32(),
+            comma_separated(comp.caps_during_update)
+        );
+    }
+}
 
 fn main() -> std::io::Result<()> {
     let sock = mctp::MctpSocket::new()?;
 
     let dev = pldm_fw::query_device_identifiers(&sock, EID)?;
-
-    println!("device: {}", dev);
-
     let params = pldm_fw::query_firmware_parameters(&sock, EID)?;
 
-    println!("params: {:#?}", params);
+    print_device_info(&dev, &params);
 
     Ok(())
 }
